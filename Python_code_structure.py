4eda83804290adcd70d0ed0659f0303ac37c1bfa
Quantumizers <quantumizers.io@gmail.com>
2026-09-11 19:49:40 +0530

    Add files via upload

---
+++ Python_code_structure.py
@@ -0,0 +1,12 @@
+from anuastra import Client
+
+astra = Client(api_key='API Key')
+
+# Define industry-standard OpenQASM 2.0
+qasm_str = """
+# Paste your QASM code here
+
+"""
+
+result = astra.execute_qasm(qasm_str, shots=1024)
+print("Results Counts:", result['results']['counts'])
